336c1e2353a3d9763ecf6b7db192e84c5da2ad24
Drksekhar <ksekharabcd@gmail.com>
2023-12-24 21:05:04 +0300

    Displaying  the menu problem fixed by Sufyan Sir

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import About from './Components/About'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom'
 import Contact from './Components/Contact'
 import Services from './Components/Services'
 import States from './Components/States'
@@ -8,25 +8,27 @@ import NoPage from './Components/NoPage'
 
 export default function App() {
   return (<>
+    {/* <h1> It is the home page</h1> */}
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainMenu />} />
-        {/* <Route index element={<States />} /> */}
-        <Route index element={<States />} />
-        <Route path='states' element={<States />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='services' element={<Services />} />
+        <Route path='/' element={<MainMenu />} >
+          <Route index element={<States />} />
+          <Route path='states' element={<States />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='services' element={<Services />} />
 
-        <Route path='*' element={<NoPage />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </>)
+
 }
 function MainMenu() {
   return (
     <div>
-      <h1>It is the Home Page</h1>
+      <br />
       <Link to='/'>
         <button>HOME</button>
       </Link>
@@ -42,6 +44,8 @@ function MainMenu() {
       <Link to='/states'>
         <button>States</button>
       </Link>
+      <Outlet />
+
     </div>
   )
 
