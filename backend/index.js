956934a30b2bdc4718f8a948e6f0004bce2d7356
Drksekhar <ksekharabcd@gmail.com>
2024-01-11 11:48:15 +0300

    inserting  data into table  - get post

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mysql = require("mysql")
+const sqlite3 = require("sqlite3")
+const db = new sqlite3.Database('./mydb.db')
 
 const app = express()
 app.use(cors())
@@ -28,4 +31,23 @@ app.get('/test2', (req, res) => {
 app.post('/test3', (req, res) => {
   const { add, ph } = req.body
   return res.json({ add, ph })
+})
+
+app.get('/student', (req, res) => {
+  const sql = "SELECT * FROM student"
+  db.all(sql, (error, result) => {
+    return res.json(result)
+  })
+})
+
+app.post('/student', (req, res) => {
+  const { name, phone } = req.body
+  const sql = "INSERT INTO student(name,phone) values (?,?)"
+  db.run(sql, [name, phone], (error) => {
+    if (error) {
+      return res.json(error.message)
+    }
+    //return res.json(name + '  added')
+    return res.json(`${name}  Name  ${phone} and Phone number added successfully`)
+  })
 })
